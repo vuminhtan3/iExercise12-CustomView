f2b3b7c767949d43f1c79288d2bcf02f6fee050a
Minh Tan Vu <vuminhtan.nuce3@gmail.com>
2023-05-24 08:29:57 +0700

    Complete basic design Screen 1

--- iExercise12-CustomView-View/ViewController.swift
+++ iExercise12-CustomView-View/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var personalDataStackView: UIStackView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Tạo một UITapGestureRecognizer
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(stackViewTapped(_:)))
+        
+        // Đặt số lần chạm cần nhận dạng (ví dụ: 1)
+        tapGesture.numberOfTapsRequired = 1
+        
+        // Thêm UITapGestureRecognizer vào UIStackView
+        personalDataStackView.addGestureRecognizer(tapGesture)
+        personalDataStackView.isUserInteractionEnabled = true
     }
-
+    @objc func stackViewTapped(_ sender: UITapGestureRecognizer) {
+       
+        let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let secondVC = mainStoryboard.instantiateViewController(withIdentifier: "PersonalVC") as! PersonalViewController
+        
+        secondVC.modalPresentationStyle = .popover
+        self.present(secondVC, animated: true)
+    }
 
 }
 
